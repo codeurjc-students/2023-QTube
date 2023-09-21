e3ce1623c2679fe1934368a09b6427959d0d73f0
Antonio José Alanís Bernal <aj.alanis.2020@alumnos.urjc.es>
2023-09-21 20:30:17 +0200

    chore: Initialize backend

---
+++ backend/QTube/src/test/java/com/example/qtube/QTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
