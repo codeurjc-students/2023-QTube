63a58983ed04ff4aecbff021926a9da14db8d8ad
Antonio José Alanís Bernal <antoniojalanis7131@gmail.com>
2023-09-24 12:05:34 +0200

    fix: Correct default colors and animations performance

--- frontend/QTube/tailwind.config.js
+++ frontend/QTube/tailwind.config.js
@@ -4,18 +4,21 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    colors: {
-      "qt-black": "#0F0F0F",
-      "qt-black-2": "#121212",
-      "qt-black-3": "#222222",
-
-      "qt-gray": "#272727",
-      "qt-gray-2": "#3F3F3F",
-      "qt-gray-3": "#888888",
-
-      "qt-blue": "#065FD4",
+    extend: {
+      colors: {
+        "qt-black": "#0F0F0F",
+        "qt-black-2": "#121212",
+        "qt-black-3": "#222222",
+  
+        "qt-gray": "#272727",
+        "qt-gray-2": "#3F3F3F",
+        "qt-gray-3": "#888888",
+  
+        "qt-blue": "#065FD4",
+      },
     },
-    extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animated")
+  ],
 }
