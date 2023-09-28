cce1e51326ddc8e3b5bba47ec22002d909ce8ec3
Antonio José Alanís Bernal <antoniojalanis7131@gmail.com>
2023-09-28 15:34:01 +0200

    feat: Add mobile-bar component

--- frontend/QTube/src/app/shared/components/bars/mobile-bar/mobile-bar.component.ts
+++ frontend/QTube/src/app/shared/components/bars/mobile-bar/mobile-bar.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { SVG_PATH } from 'src/app/core/constants/imagePaths';
 
 @Component({
   selector: 'app-mobile-bar',
   templateUrl: './mobile-bar.component.html',
-  styleUrls: ['./mobile-bar.component.css']
+  styleUrls: ['./mobile-bar.component.css'],
 })
 export class MobileBarComponent {
+  readonly svgPath = SVG_PATH;
 
+  navigationElements = [
+    {
+      svg: 'home.svg',
+      link: '/',
+      title: 'Home',
+    },
+    {
+      svg: 'subscriptions.svg',
+      link: '/subscriptions',
+      title: 'Subscriptions',
+    },
+    {
+      svg: 'tendencies.svg',
+      link: '/tendencies',
+      title: 'Tendencies',
+    },
+    {
+      svg: 'playlists.svg',
+      link: '/playlists',
+      title: 'Playlists',
+    },
+  ];
 }
--- frontend/QTube/src/app/shared/components/buttons/bar-button/bar-button.component.ts
+++ frontend/QTube/src/app/shared/components/buttons/bar-button/bar-button.component.ts
@@ -11,4 +11,18 @@ export class BarButtonComponent {
 
   @Input()
   title = '';
+
+  @Input()
+  type = '';
+
+  buttonClasses: { [key: string]: string } = {
+    drawer:
+      '@container/drawer-button w-full px-drawer-x py-drawer-y @drawer-xs/drawer:flex @drawer-xs/drawer:items-center @drawer-xs/drawer:gap-[1.375rem] rounded-xl',
+    'mobile-bar': 'w-full p-2 flex flex-col items-center gap-1',
+  };
+
+  titleClasses: { [key: string]: string } = {
+    drawer: 'hidden @drawer-xs/drawer-button:block text-sm',
+    'mobile-bar': 'text-[10px]',
+  };
 }
